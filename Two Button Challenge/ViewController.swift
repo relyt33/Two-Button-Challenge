dc507f558e23055ca2dbc9de1a59d9d581693a01
Tyler Thames <tylerthames@bellsouth.net>
2022-05-19 07:18:03 -0500

    Initial Commit

---
+++ Two Button Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Two Button Challenge
+//
+//  Created by Tyler Thames on 5/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
